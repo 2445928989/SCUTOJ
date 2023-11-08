2672ae2924a2dcd2cea8d902ebd5cbb1fee61abd
失格绅士 <121372130+2445928989@users.noreply.github.com>
2023-11-08 17:18:28 +0800

    Update B.cpp

--- 23级计算机类1班——第4章课后题/B.cpp
+++ 23级计算机类1班——第4章课后题/B.cpp
@@ -11,27 +11,28 @@
 11
 17 31 13 71*/
 
-#include <iostream>
-#include<algorithm>
-#include<map>
-#include<string>
-#include<math.h>
-#include<iomanip>
+#include<iostream>
 using namespace std;
 int a[10];
 int main()
 {
-    while (true) {
-        for (int i = 0; i < 10; i++)cin >> a[i];
+    while (cin >> a[0]) {
+        for (int i = 1; i < 10; i++)cin >> a[i];
         for (int i = 0; i < 10; i++) {
             bool flag = 0;
             for (int j = 2; j * j <= a[i]; j++) {
                 if (a[i] % j == 0) {
                     flag = 1;
+                    break;
                 }
             }
             if (flag == 0)cout << a[i] << " ";
         }
         cout << endl;
     }
-}
+    return 0;
+}/*注意这题while()里面不能写true或者1！
+学校的OJ跳出循环的方式是输入一个非法的数据或者类似于EOF那样的东西，
+如果写true而不是cin>>a[0]，他就会认为你只是把这个非法的数据输入进了a[0]里面，
+while循环会继续运作而不是跳出，导致答案错误WA！
+太抽象了！！！让我交了几十次！！*/
